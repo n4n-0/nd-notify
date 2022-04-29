034036f0b7dacff01dee4e7fb0a9b094135cd9dc
JericoFX <56100605+JericoFX@users.noreply.github.com>
2022-04-29 15:27:10 -0300

    Need Testing
    Refractor Code
    * Add default style if someone pass the wrong style.
    * Add function to get icon so no more switch.

--- html/js/main.js
+++ html/js/main.js
@@ -1,50 +1,44 @@
-
-// $(document).ready(function() {
-//     ShowNotification({
-//         type: 'inform',
-//         text: 'This is a test notification.',
-//         length: 5000
-//     })
-// });
-
-window.addEventListener('message', function(e) {
-    ShowNotification(e.data);
-})
+const ICONS_TYPES = {
+  'primary': '<i class="fa-solid fa-circle-info"></i>',
+  "error": '<i class="fa-solid fa-circle-exclamation"></i>',
+  "success": '<i class="fa-solid fa-check"></i>',
+  "police": '<i class="fa-solid fa-handcuffs"></i>'
+}
+const ICON_DEFAULT_TYPE = 'primary'
+const STYLES_TYPES = ['primary',"error","success", "police"]
+function escapeHtml(unsafe)
+{
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
 
 const CreateNotification = (data) => {
-    var $notification = $(document.createElement('div'));
-    switch (data.type) {
-        case 'primary':
-            var $icon = '<i class="fa-solid fa-circle-info"></i>'
-            break;
-        case 'error':
-            var $icon = '<i class="fa-solid fa-circle-exclamation"></i>'
-            break;
-        case 'success':
-            var $icon = '<i class="fa-solid fa-check"></i>'
-            break;
-        case 'police':
-            var $icon = '<i class="fa-solid fa-handcuffs"></i>';
-            break;
-    }
-    $notification.addClass('notification').addClass(data.type);
-    $notification.html($icon + ' ' + data.text);
-    $notification.fadeIn(1000);
-    if (data.style !== undefined) {
-        Object.keys(data.style).forEach(function(css) {
-            $notification.css(css, data.style[css])
-        });
-    }
-
-    return $notification;
+  const $notification = $(document.createElement('div'));
+   const l = STYLES_TYPES.find(e => e === data.type) ? data.type : ICON_DEFAULT_TYPE
+  const $icon = ICONS_TYPES.hasOwnProperty(data.type) === true ? ICONS_TYPES[data.type] : ICONS_TYPES[ICON_DEFAULT_TYPE]
+  const TEXT = escapeHtml(data.text)
+  $notification.addClass('notification').addClass(l);
+  $notification.html($icon + ' ' + TEXT);
+  $notification.fadeIn(1000);
+  if (data.style !== undefined) {
+    Object.keys(data.style).forEach(function(css) {
+      $notification.css(css, data.style[css])
+    });
+  }else{
+   $notification.addClass(l)
+  }
+  return $notification;
 }
-
 const ShowNotification = (data) => {
-    var $notification = CreateNotification(data);
-    $('.notification_container').append($notification);
-    setTimeout(function() {
-        $.when($notification.fadeOut()).done(function() {
-            $notification.remove()
-        });
-    }, data.length != null ? data.length : 2500);
+  const $notification = CreateNotification(data);
+  $('.notification_container').append($notification);
+  setTimeout(function() {
+    $.when($notification.fadeOut()).done(function() {
+      $notification.remove()
+    });
+  }, data.length != null ? data.length : 2500);
 }
